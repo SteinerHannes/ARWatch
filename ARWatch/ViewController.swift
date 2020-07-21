3e45d5e176ef65dce2c88150e87ce811a043c8ca
Hannes Steiner <Hannes.Steiner@web.de>
2020-07-21 10:44:26 +0200

    ViewController setup complete

--- ARWatch/ViewController.swift
+++ ARWatch/ViewController.swift
@@ -15,110 +15,69 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    public var standardNode: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("load")
-        // Set the view's delegate
         sceneView.delegate = self
-        
         sceneView.showsStatistics = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        
-        // Create a session configuration
         let configuration = ARImageTrackingConfiguration()
-        
         if let imagesToTrack = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main) {
-            
             configuration.trackingImages = imagesToTrack
-            
-            // this tells ARKit how many images it is supposed to track simultaneously, ARKit can do upto 100
             configuration.maximumNumberOfTrackedImages = 1
         }
-        
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
     
     // MARK: - ARSCNViewDelegate
     
-    
-    //Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-        
-        // Cast the found anchor as image anchor
         guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
-        
-        // get the name of the image from the anchor
         guard let imageName = imageAnchor.name else { return nil }
-        
-        // Check if the name of the detected image is the one you want
         let images = ["audio", "settings", "map"]
-        
         if images.contains(imageName) {
-            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width*3,
-                                 height: imageAnchor.referenceImage.physicalSize.height*3.6522)
-            
-            let planeNode = SCNNode(geometry: plane)
-            // When a plane geometry is created, by default it is oriented vertically
-            // so we have to rotate it on X-axis by -90 degrees to
-            // make it flat to the image detected
-            planeNode.eulerAngles.x = -.pi / 2
-            
-            createHostingController(for: planeNode)
-            
-            node.addChildNode(planeNode)
-            return node
+            guard standardNode != nil else {
+                let newNode = SCNNode()
+                let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width*3,
+                                     height: imageAnchor.referenceImage.physicalSize.height*3.6522)
+                let planeNode = SCNNode(geometry: plane)
+                planeNode.eulerAngles.x = -.pi / 2
+                createHostingController(for: planeNode)
+                newNode.addChildNode(planeNode)
+                standardNode = newNode
+                return standardNode
+            }
+            return standardNode
         } else {
             return nil
         }
     }
     
     func createHostingController(for node: SCNNode) {
-        // create a hosting controller with SwiftUI view
         let arVC = UIHostingController(rootView: ContentView())
-        
-        // Do this on the main thread
         DispatchQueue.main.async {
             arVC.willMove(toParent: self)
-            // make the hosting VC a child to the main view controller
             self.addChild(arVC)
-            
-            // set the pixel size of the Card View
             arVC.view.frame = CGRect(x: 0, y: 0, width: 200*3, height: 200*3.6522)
-            
-            // add the ar card view as a subview to the main view
             self.view.addSubview(arVC.view)
-            
-            // render the view on the plane geometry as a material
             self.show(hostingVC: arVC, on: node)
         }
     }
     
     func show(hostingVC: UIHostingController<ContentView>, on node: SCNNode) {
-        // create a new material
         let material = SCNMaterial()
-        
-        // this allows the card to render transparent parts the right way
         hostingVC.view.isOpaque = false
-        
-        // set the diffuse of the material to the view of the Hosting View Controller
         material.diffuse.contents = hostingVC.view
-        
-        // Set the material to the geometry of the node (plane geometry)
         node.geometry?.materials = [material]
-        
         hostingVC.view.backgroundColor = UIColor.clear
     }
     
